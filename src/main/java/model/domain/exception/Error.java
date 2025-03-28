59ecf1082ea6a67ddea1584be9c89982acceafb4
baaamk <brandon55321@gmail.com>
2025-03-29 02:46:44 +0900

    refactor: 오류 내역 수정

--- src/main/java/model/domain/exception/Error.java
+++ src/main/java/model/domain/exception/Error.java
@@ -5,7 +5,9 @@ public enum Error {
     INVALID_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATED_NUMBER("[ERROR] 중복된 번호가 존재합니다."),
     DUPLICATED_BONUS_NUMBER("[ERROR] 로또와 중복된 번호가 있습니다."),
-    INVALID_LOTTO_COUNT("[ERROR] 6개의 숫자를 입력해야 합니다.");
+    INVALID_LOTTO_COUNT("[ERROR] 6개의 숫자를 입력해야 합니다."),
+    INVALID_NUMBER_FORMAT("[ERROR] 숫자만 입력하세요."),
+    INVALID_PURCHASE_AMOUNT("[ERROR] 구매 금액은 0 이상이어야 합니다.");
 
     private final String errorMessage;
 
