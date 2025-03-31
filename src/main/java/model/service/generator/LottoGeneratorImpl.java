7e1f0388e8afb1b413706418338ede2739a3e285
baaamk <brandon55321@gmail.com>
2025-04-01 00:48:26 +0900

    refactor: 들여쓰기 수정

--- src/main/java/model/service/generator/LottoGeneratorImpl.java
+++ src/main/java/model/service/generator/LottoGeneratorImpl.java
@@ -6,8 +6,7 @@ import model.domain.Lotto;
 import static model.domain.unit.Unit.*;
 
 public class LottoGeneratorImpl implements LottoGenerator {
-
-        public Lotto generateLotto() {
+    public Lotto generateLotto() {
         return new Lotto(Randoms.pickUniqueNumbersInRange(MIN_NUMBER.getValue(), MAX_NUMBER.getValue(), UNIT_LOTTO_NUMBER.getValue()));
     }
 
