1c4ddf8e36a16f3768f0d75d505a25e7ac72fae2
baaamk <brandon55321@gmail.com>
2025-04-02 03:31:19 +0900

    feat: dto 생성

--- src/main/java/view/output/UserOutput.java
+++ src/main/java/view/output/UserOutput.java
@@ -1,9 +1,11 @@
 package view.output;
 
+import model.dto.LottoWinningDTO;
+
 import java.util.List;
 
 public interface UserOutput {
     void printLotto(int count, List<List<Integer>> lottoNumbersList);
 
-    void printResult(int[] matches, double percent);
+    void printResult(List<LottoWinningDTO> results, double percent);
 }
